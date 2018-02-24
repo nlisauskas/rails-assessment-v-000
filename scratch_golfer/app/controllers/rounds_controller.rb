d95696a5a589fb340b245ccef6842766db54a1bd
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-02-24 01:52:42 +0000

    Done.

--- scratch_golfer/app/controllers/rounds_controller.rb
+++ scratch_golfer/app/controllers/rounds_controller.rb
@@ -32,6 +32,29 @@ class RoundsController < ApplicationController
     @round = Round.find_by(params[:id])
   end
 
+  def edit
+    @round = Round.find_by(params[:id])
+  end
+
+  def update
+    @round = Round.find_by(params[:id])
+    @round.update(round_params)
+    @round.save
+    @user = User.find_by(session[:user_id])
+    @user.calculate_handicap
+    redirect_to @round
+  end
+
+  def destroy
+    @round = Round.find_by(params[:id])
+    if @round.user_id == session[:user_id]
+      @round.destroy
+      redirect_to user_path(session[:user_id])
+    else
+      redirect_to user_path(session[:user_id])
+    end
+  end
+
   private
 
   def round_params
